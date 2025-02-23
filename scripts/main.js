5bf815ab81e7725fa383b51b7969378a02643477
Abdul Wahab <80677064+599-wahab@users.noreply.github.com>
2025-02-23 18:34:45 +0500

    aw

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@
   const CANVASHEIGHT = 150
   const CANVASID = 'canvas'
 
-  let texts = ['MY DEAR', 'LOOK UP', 'AT THE', 'STARRY SKY', 'ARE YOU', 'LOOKING',  'AT THE', 'SAME STAR', 'WITH ME ?', 'HAPPY', 'BIRTHDAY 🧁', 'DAY', 'KHADIJA G', 'Ap MaShaAllah ', 'Ks Ap MaShaAllah']
+  let texts = ['MY DEAR', 'LOOK UP', 'AT THE', 'STARRY SKY', 'ARE YOU', 'LOOKING',  'AT THE', 'SAME STAR', 'WITH ME ?', 'HAPPY', 'BIRTHDAY 🧁', 'DAY', 'KHADIJA G', 'Ap MaShaAllah ', 'AW = KS']
 
   let canvas,
     ctx,
